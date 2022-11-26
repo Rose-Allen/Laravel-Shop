67ffc09d064cb293389d217a40091d09c938ecf8
Dimash <d.esengeldiev@mail.ru>
2022-11-26 23:51:23 +0600

    livewire product add

--- resources/views/frontEnd/collection/products/index.blade.php
+++ resources/views/frontEnd/collection/products/index.blade.php
@@ -19,46 +19,7 @@
                 <div class="col-md-12">
                     <h4 class="mb-4">Наши Товары</h4>
                 </div>
-                @forelse($products as $product)
-                    <div class="col-md-3">
-                        <div class="product-card">
-                            <div class="product-card-img">
-                                @if($product->quantity > 0)
-                                    <label class="stock bg-success">In Stock</label>
-                                @else
-                                    <label class="stock bg-success">Out of Stock</label>
-                                @endif
-                                @if($product->productImages->count() > 0)
-                                    <a href="{{url('/collections/'.$product->category->slug.'/'.$product->slug)}}">
-                                        <img src="{{ asset($product->productImages[0]->image) }}"
-                                             alt="{{$product->name}}">
-                                    </a>
-                                @endif
-                            </div>
-                            <div class="product-card-body">
-                                <p class="product-brand">{{$product->brand}}</p>
-                                <h5 class="product-name">
-                                    <a href="{{url('/collections/'.$product->category->slug.'/'.$product->slug)}}">
-                                        {{$product->name}}
-                                    </a>
-                                </h5>
-                                <div>
-                                    <span class="selling-price">{{$product->selling_price}}₸</span>
-                                    <span class="original-price">{{$product->original_price}}₸</span>
-                                </div>
-                                {{--                            <div class="mt-2">--}}
-                                {{--                                <a href="" class="btn btn1">Add To Cart</a>--}}
-                                {{--                                <a href="" class="btn btn1"> <i class="fa fa-heart"></i> </a>--}}
-                                {{--                                <a href="" class="btn btn1"> View </a>--}}
-                                {{--                            </div>--}}
-                            </div>
-                        </div>
-                        @empty
-                            <div style="margin-top: 100px">
-                                <h1 class="text-center"> Товар {{$category->name}} не найдены!</h1>
-                            </div>
-                    </div>
-                @endforelse
+                @livewire('front-end.product.index', ['products' => $products, 'category'=>$category] )
             </div>
         </div>
     </div>
