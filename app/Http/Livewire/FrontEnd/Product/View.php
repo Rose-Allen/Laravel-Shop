31bf458c360bccc009021007be8955b4c83fe760
Dimash <d.esengeldiev@mail.ru>
2023-02-20 23:34:28 +0600

    stock and outofstock  26

--- app/Http/Livewire/FrontEnd/Product/View.php
+++ app/Http/Livewire/FrontEnd/Product/View.php
@@ -6,8 +6,15 @@ use Livewire\Component;
 
 class View extends Component
 {
-    public $category, $product;
+    public $category, $product, $productColorSelectedQantity ;
 
+    public function colorSelected($productColorId){
+       $productColor =  $this->product->productColors()->where('id', $productColorId)->first();
+       $this->productColorSelectedQantity =  $productColor->quantity;
+       if($this->productColorSelectedQantity == 0){
+           $this->productColorSelectedQantity = 'outOfStock';
+       }
+    }
     public function mount($category,$product ){
         $this->product = $product;
         $this->category = $category;
--- resources/views/livewire/frontend/product/view.blade.php
+++ resources/views/livewire/frontend/product/view.blade.php
@@ -26,16 +26,42 @@
                             <span class="original-price">{{$product->original_price}}₸</span>
                         </div>
                         <div>
-                            @if($product->productColors)
-                                @foreach($product->productColors as $ColorItem)
-                                    <input type="radio" name="colorSelection" value="{{$ColorItem->id}}">{{$ColorItem->color->name}}
-                                @endforeach
+                            @if($product->productColors->count() > 0)
+                                @if($product->productColors)
+                                    @foreach($product->productColors as $ColorItem)
+                                        {{--                                        <input type="radio" name="colorSelection"--}}
+                                        {{--                                               value="{{$ColorItem->id}}">{{$ColorItem->color->name}}--}}
+                                        <label class="colorSelectionLabel"
+                                               style="background-color: {{$ColorItem->color->code}}"
+                                               wire:click="colorSelected({{$ColorItem->id}})"
+                                        >
+
+                                            {{$ColorItem->color->name}}
+                                        </label>
+
+                                    @endforeach
+                                @endif
+                                <div>
+                                    @if($this->productColorSelectedQantity == "outOfStock")
+                                        <label class="btn-sm  px-1 py-1 mt-2 text-white bg-danger">Out of Stock</label>
+                                    @elseif($this->productColorSelectedQantity > 0)
+                                        <label class="btn-sm px-1 py-1 mt-2 text-white bg-success">In Stock</label>
+                                    @endif
+                                </div>
+
+                            @else
+                                @if($product->quantity)
+
+                                    <label class="btn-sm px-1 py-1 mt-2 text-white bg-success">In Stock</label>
+                                @else
+                                    <label class="btn-sm  px-1 py-1 mt-2 text-white bg-danger">Out of Stock</label>
+                                @endif
                             @endif
                         </div>
                         <div class="mt-2">
                             <div class="input-group">
                                 <span class="btn btn1"><i class="fa fa-minus"></i></span>
-                                <input type="text" value="1" class="input-quantity" />
+                                <input type="text" value="1" class="input-quantity"/>
                                 <span class="btn btn1"><i class="fa fa-plus"></i></span>
                             </div>
                         </div>
